a27359a646f13be32f3774788cf4274b5f2ad66a
thakurtejveer <thakurtejveer1@gmail.com>
2019-10-25 12:51:59 +0530

    class binding completed

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -6,27 +6,46 @@ import { Component, OnInit } from '@angular/core';
   <h1>
   Welcome {{name}}
   </h1>
-  <h2>{{2+2}}</h2>
-  <h2>{{name.toUpperCase()}}</h2>
-  <h2>{{name.length}}</h2>
-  <h2>{{"Welcome "+name+ " to Inter IIT Sports Meet"}}</h2>
-  <h2>{{greetUser()}}</h2>
-  <h2>{{a}}</h2>
-  <h2>{{siteUrl}}</h2>
+  <h2 class="text-success">{{"Your name in Upper Case is looks like :  "+name.toUpperCase()}}</h2>
+  // <h2 [class]=successClass>{{name.length}}</h2>
+
+  <h2 class="text-special" [class]=successClass>{{"Welcome "+name+ " to Inter IIT Sports Meet"}}</h2>
+  <h2 [class.text-danger]="hashError">{{greetUser()}}</h2>
   <input [id]="myId" type="text" value="tezaaa">
   <input [disabled]="isDisabled" id ="{{myId}}" type="text" value="singh">
   <input bind-disabled="isDisabled" id ="{{myId}}" type="text" value="singh">
+  <h2 [ngClass]="messageClasses" >Hey there, I won the race.. hurraaaaah</h2>
 
 
   `,
-  styleUrls: []
+  styles: [`
+  .text-success {
+    color: green;
+  }
+  .text-danger {
+    color: red;
+  }
+  .text-special {
+    font-style: italic;
+    color:blue;
+  }
+  `
+
+  ]
 })
 export class TestComponent implements OnInit {
   public name="Tejveer Singh";
-  public a=5+6;
-  public siteUrl=window.location.href;
   public myId="testId";
   public isDisabled = false;
+  public successClass="text-success";
+  public hashError=false;
+  public isSpecial=true;
+  public messageClasses ={
+    "text-special" :this.isSpecial,
+    "text-success":!this.hashError,
+    "text-danger" :!this.hashError
+
+  }
 
   constructor() { }
 
